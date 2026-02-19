df8c191ed93da5f5981577c1e9b047715f5a9bb3
LordSox <mi.ochoadr@hotmail.com>
2026-02-18 21:09:43 -0600

    cor

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        graduaciones: 'graduaciones.html',
+        bodas: 'bodas.html',
+        estolas: 'estolas.html',
+        empresas: 'empresas.html',
+        trayectoria: 'trayectoria.html',
+        contacto: 'contacto.html',
+        politicas: 'politicas.html',
+      },
+    },
+  },
 })
